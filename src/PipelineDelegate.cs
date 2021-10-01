27dadab9a9e08080af77e0387e71c6127d33ef15
Dan <daningalla@gmail.com>
2021-10-01 17:18:02 -0600

    In progress

--- src/PipelineDelegate.cs
+++ src/PipelineDelegate.cs
@@ -1,10 +1,7 @@
-﻿namespace Vertical.Pipelines
+using System;
+using System.Threading.Tasks;
+
+namespace Vertical.Pipelines
 {
-    /// <summary>
-    /// Defines a delegate that is used to route control flow and state
-    /// throughout an ordered pipeline of tasks.
-    /// </summary>
-    /// <typeparam name="TContext">The type of context being shared among pipeline tasks.</typeparam>
-    /// <typeparam name="TResult">The type of result produced by the pipeline.</typeparam>
-    public delegate TResult PipelineDelegate<in TContext, out TResult>(TContext context);
+    public delegate Task PipelineDelegate<in TContext>(TContext context, IServiceProvider serviceProvider);
 }
